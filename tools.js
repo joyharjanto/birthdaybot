20530d3e3c9f1a024390e98617301470fd9d14ce
Joy Harjanto <38966343+joyharjanto@users.noreply.github.com>
2021-01-24 21:24:05 -0500

    Update tools.js

--- tools.js
+++ tools.js
@@ -1,30 +1,29 @@
-function testsendEmails() {
-  var sheet = SpreadsheetApp.getActiveSheet(); // connecting sheet
-  var startRow = 2; // first row
-  var numRows = sheet.getLastRow(); // last row
-  var dataRange = sheet.getRange(startRow, 1, numRows, sheet.getLastColumn()); // get data in range
-  var data = dataRange.getValues();
-  console.log(data)
-
-  for(i in data){
-    var row = data[i]; // one data row
-    var date = new Date(); // today's date
-    var sheetDate = new Date(row[3]); // actual date
-    Sdate = Utilities.formatDate(date,'GMT-0500','yyyy:MM:dd')
-    SsheetDate = Utilities.formatDate(sheetDate,'GMT-0500', 'yyyy:MM:dd')
-    Logger.log(Sdate+' =? '+SsheetDate)
-    console.log(Sdate === SsheetDate)
-    
-    if(Sdate === SsheetDate){
-        var email = row[0];
-        var name = row[1];      
-        var actualdate = Utilities.formatDate(row[2],'GMT-0500','MM:dd');
-        var event = row[4];
-        var subject = 'Special day reminder';
-        var todaysDate = Sdate;
-        var message = "Hey " + yourName + "!" + " Today is the "+ Sdate+ ". It is " + name + "'s " + event + " on " + actualdate;
-        console.log(message)
-        MailApp.sendEmail(email, subject, message);
-    }
-  }
+function sendEmails() {
+ var sheet = SpreadsheetApp.getActiveSheet(); // connecting sheet
+ var startRow = 2; // first row
+ var numRows = sheet.getLastRow(); // last row
+ var dataRange = sheet.getRange(startRow, 1, numRows, sheet.getLastColumn()); // get data in range
+ var data = dataRange.getValues();
+ var yourName = "Caitlyn Tjahjadi"
+ 
+ for(i in data){
+   var row = data[i]; // one data row
+   var date = new Date(); // today's date
+   var sheetDate = new Date(row[3]); // actual date
+   Sdate = Utilities.formatDate(date,Session.getScriptTimeZone(),'yyyy/MM/dd')
+   SsheetDate = Utilities.formatDate(sheetDate,Session.getScriptTimeZone(), 'yyyy/MM/dd')
+ 
+   if(Sdate === SsheetDate){
+     console.log("accurate")
+     var email = row[0];
+     var name = row[1];      
+     var actualdate = Utilities.formatDate(row[2],Session.getScriptTimeZone(),'MM/dd');
+     var event = row[4];
+     var subject = 'Special day reminder';
+     var todaysDate = Sdate;
+     var message = "Hey " + yourName + "!" + " Today is the "+ Sdate+ ". It is " + name + "'s " + event + " on " + actualdate;
+     console.log(message)
+     MailApp.sendEmail(email, subject, message);
+   }
+ }
 }
